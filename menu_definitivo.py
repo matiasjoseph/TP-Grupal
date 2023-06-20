4b0f9d78bab45eb38762c8473c89f58c92286715
matiasjoseph <matiasljoseph@gmail.com>
2023-06-20 20:37:20 -0300

    Modificación del menu

--- menu_definitivo.py
+++ menu_definitivo.py
@@ -1,3 +1,17 @@
+class Empleado:
+    def __init__(self, nombre, clave):
+        self.nombre = nombre
+        self.clave = clave
+
+    def __str__(self):
+        return f'Nombre: {self.nombre}'
+
+    def generar_dic(self):
+        return {'nombre': self.nombre, 'clave': self.clave}
+
+    def cambiar_clave(self, cosa):
+        self.clave = cosa
+
 class Comida:
     def __init__(self, Nombre, descripcion, precio):
         self.precio = precio
@@ -31,10 +45,15 @@ def agregar_a_carta(pro, cant):
             a.cantidad -= cant
 
 
+def agregar_empleado(nom, psw):
+    laburante = Empleado(nom, psw)
+    empleados.append(laburante)
+
+
 def eliminar_de_carta(comida):
     for i in stock:
         if i.nombre == comida:
-            cocina.remove(i)
+            stock.remove(i)
 
 
 def pagar():
@@ -54,6 +73,10 @@ def hacer_visible(elemento):
         for prod in stock:
             listita.append(prod.generar_dic())
         print(listita)
+    else:
+        for emp in empleados:
+            listita.append(emp.generar_dic())
+        print(listita)
 
 def salir():
     print('Gracias por utilizar nuestro servicios')
@@ -62,18 +85,32 @@ def salir():
 
 cocina = []
 
+empleados = []
+
 
 while True:
+    agregar_empleado('gerente', '1234')
     stock = []
     print('\nBienvenido a la Rotiseria')
+
     try:
-            while True:
-                print('\nQue accion desea realizar')
-                print('1) Revisar stock')
-                print('2) Agregar a la carta')
-                print('3) Eliminar de la carta')
-                print('4) Cambiar dato de comida')
-                print('5) Salir')
+
+        print('\nNombre')
+        usr = input(">> ")
+        print("\nIngresar su clave")
+        pasw = input(">> ")
+
+        for empleado in empleados:
+            if empleado.nombre == usr and empleado.clave == pasw:
+                while True:
+                    print('\nQue accion desea realizar')
+                    print('1) Revisar stock')
+                    print('2) Agregar a la carta')
+                    print('3) Eliminar de la carta')
+                    print('4) Cambiar dato de comida')
+                    print('5) Pagar')
+                    print('6) Salir')
+
 
                 try:
                     num = int(input('>> '))
@@ -81,19 +118,25 @@ while True:
                     if num == 1:
                         hacer_visible('cocina')
                     elif num == 2:
-                        agregar_a_carta(input('Nombre '), input('Cantidad '))
+                        agregar_a_carta(input('Nombre del Plato: '), input('Cantidad: '))
                     elif num == 3:
                         hacer_visible('stock')
-                    elif num == 4:
-                        eliminar_de_carta(input('Producto '))
+                    elif numero == 4:
+                        prod = input('Ingrese producto ')
+                        for i in gondola:
+                            if i.nombre == prod:
+                                i.cambiar_informacion(input('Que desea cambiar '), input('>> '))
                     elif num == 5:
+                        pagar()
+                    elif num == 6:
                         salir()
                         break
                     else:
                         break
                 except:
                     print("\nDebe ingresar numeros, no letras")
-
+            else:
+                print('Usuario o clave no validos. Ingrese de nuevo')
 
     except:
         print("\nDebe ingresar numeros, no letras")
