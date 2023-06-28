9e787fe821d6dc09fbab45065e6fbb3a6e537d15
matiasjoseph <matiasljoseph@gmail.com>
2023-06-28 14:41:31 -0300

    Arreglo de funciones del menu

--- menu_variable.py
+++ menu_variable.py
@@ -1,19 +1,15 @@
 class Comida:
-    def __init__(self, nombre, descripcion, precio):
-        self.precio = precio
+    def __init__(self, nombre, cantidad, precio):
         self.nombre = nombre
-        self.descripcion = descripcion
-
+        self.cantidad = cantidad
+        self.precio = precio
 
     def generar_dic(self):
-        return {'nombre': self.nombre, 'descripcion': self.descripcion, 'precio': self.precio}
+        return {'nombre': self.nombre, 'cantidad': self.cantidad, 'precio': self.precio}
 
-    def cambiar_informacion(self, dato, plato):
-        if dato == 'nombre':
-            self.nombre = plato
 
     def __str__(self):
-        return f'nombre: {self.nombre}\ndescripcion: {self.descripcion} \nPrecio: {self.precio} pesos'
+        return f'nombre: {self.nombre}\ncantidad: {self.cantidad} \nprecio: {self.precio}'
 
     def informacion(self):
         return print(self)
@@ -21,17 +17,17 @@ class Comida:
 
 def agregar_a_carta(pro, cant, precio):
     cant = int(cant)
-    for a in cocina:
-        if a.nombre == pro and a.cantidad > cant:
-            val = a.precio
-            item = Comida(pro, cant, val)
-            stock.append(item)
-            a.cantidad -= cant
+    # for a in cocina:
+    # if a.nombre == pro and a.cantidad > cant:
+    val = precio
+    item = Comida(pro, cant, val)
+    print(item)
+    stock.append(item)
+    # a.cantidad -= cant
 
-def eliminar_de_carta(comida):
-    for i in stock:
-        if i.nombre == comida:
-            cocina.remove(i)
+
+
+
 
 def hacer_visible(elemento):
     listita = []
@@ -44,6 +40,7 @@ def hacer_visible(elemento):
             listita.append(prod.generar_dic())
         print(listita)
 
+
 def pagar():
     total = 0
     for i in stock:
@@ -52,53 +49,68 @@ def pagar():
     print(f'Usted debe pagar {total} pesos')
 
 
-
 def salir():
     print('Gracias por utilizar nuestro servicios')
 
+def cambiar_informacion(prod, new):
+    j = 0
+    for i in stock:
+
+        if stock[j].nombre == prod:
+            stock[j].nombre = new
+    j = j+1
+
+def eliminar_de_carta(comida):
+    for i in stock:
+        if i.nombre == comida:
+            stock.remove(i)
+
 cocina = []
+stock = []
 
 Flag = True
 
 while Flag:
-    stock = []
+
     print('\nBienvenido a la Rotiseria')
     try:
 
         while Flag:
-                print('\nQue accion desea realizar?')
-                print('1) Revisar Pedido')
-                print('2) Agregar Cosas al Pedido')
-                print('3) Eliminar Cosas del Pedido')
-                print('4) Cambiar dato del Pedido')
-                print('5) Pagar')
-                print('6) Salir')
+            print('\nQue accion desea realizar?')
+            print('1) Revisar Pedido')
+            print('2) Agregar Cosas al Pedido')
+            print('3) Eliminar Cosas del Pedido')
+            print('4) Cambiar Nombre del Pedido')
+            print('5) Pagar')
+            print('6) Salir')
 
-                try:
-                    num = int(input('>> '))
+            try:
+                num = int(input('>> '))
 
-                    if num == 1:
-                        hacer_visible('cocina')
-                    elif num == 2:
-                        agregar_a_carta(input('Nombre del plato: '), int(input('Cantidad: ')), int(input('Precio: ')))
-                    elif num == 3:
-                        eliminar_de_carta(input('Que plato desea eliminar?: '))
-                    elif num == 4:
-                        prod = input('Ingrese el nombre del plato para cambiar:  ')
-                        for i in cocina:
-                            if i.nombre == prod:
-                                i.cambiar_informacion(input('Que desea cambiar?: '), input('>> '))
-                    elif num == 5:
-                        pagar()
+                if num == 1:
+                    hacer_visible('stock')
+                elif num == 2:
+                    agregar_a_carta(input('Nombre del plato: '), int(input('Cantidad: ')), int(input('Precio: ')))
+                elif num == 3:
+                    eliminar_de_carta (input('Que plato desea eliminar?: '))
+                elif num == 4:
 
-                    elif num == 6:
-                        salir()
-                        Flag = False
-                    else:
-                        break
-                except:
-                    print("\nDebe ingresar numeros, no letras")
+                    prod = input('Ingrese el nombre del plato para cambiar:  ')
+                    new = input('Ingrese el nombre del nuevo plato:  ')
+                    cambiar_informacion(prod, new)
+
+                elif num == 5:
+                    pagar()
+
+                elif num == 6:
+                    salir()
+                    Flag = False
+                else:
+                    break
+            except:
+                print("\nDebe ingresar numeros, no letras")
 
 
     except:
-            print("\nDebe ingresar numeros, no letras")
+        print("\nDebe ingresar numeros, no letras")
+
